77b7709bff731e0728e4d4b1b925d94e17c331dc
Anjanee Wijewardana <anjaneew@gmail.com>
2025-03-31 17:44:48 +0300

    lesson 2 variables

---
+++ 2/index.js
@@ -0,0 +1,91 @@
+// //Declaration
+//
+// let x;
+// let y;
+//
+// //Assignment
+//
+// x = 100;
+// y = 200;
+//
+// console.log(x);
+//
+// // Declaration and assignment together
+//
+// let z = 50;
+// console.log(z);
+//
+//
+// //Data types
+//
+// //Number
+//
+// let age = 25;
+// let price = 10.99;
+// let gpa = 3.7;
+//
+// console.log(age);
+// console.log(typeof gpa);
+// console.log(price);
+//
+// console.log(`I am ${age} years old.`);
+// console.log(`The price is $${price}.`);
+// console.log(`Your gpa is ${gpa}. This is great.`);
+//
+// // String
+//
+// let firstName = "Anjanee";
+// let lastName = "Wijewardana";
+// let favouriteFood = "milkrice";
+// let email = "myEmail.com";
+//
+// console.log(typeof firstName);
+// console.log(`Your name is ${firstName}`);
+// console.log(typeof lastName);
+// console.log(`Your last name is ${lastName}`);
+// console.log(`You like ${favouriteFood}`);
+// console.log(`Your email is ${email}`);
+//
+// //Booleans
+// let online = true;
+// console.log(typeof online);
+// console.log(`Bro is online: ${online}`);
+//
+// let forSale = true;
+// console.log(`Is this car is for sale?: ${forSale}`);
+//
+// let isStudent = true;
+// console.log(`Is he a student? : ${isStudent}`);
+//
+// if (isStudent){
+//     console.log("welcome to the summer school");
+// } else {
+//     console.log("You are not enrolled yet.");
+// }
+//
+
+let fullName = "Anjanee S. Wijewardana";
+let age = 25;
+let isStudent = true;
+
+document.getElementById("p1").textContent = `your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `You are enrolled in school: ${isStudent}`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
