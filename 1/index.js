22f8d9410c7eb38b970e05af4c6c032266d95e35
Anjanee Wijewardana <anjaneew@gmail.com>
2025-03-31 12:26:47 +0300

    lesson 1 done

---
+++ 1/index.js
@@ -0,0 +1,17 @@
+console.log("Hello");
+console.log("I like Pizza");
+
+window.alert("This is an alert!");
+window.alert("I like Pizza!");
+
+//COMMENTS
+
+/* this is
+a  comment
+ */
+
+
+document.getElementById("myH1").textContent= "Hello";
+document.getElementById("myP").textContent = "I love Pizza!I love Pizza!I love Pizza!I love Pizza!I love Pizza!I love Pizza!I love Pizza!I love Pizza!I love Pizza!";
+
+
