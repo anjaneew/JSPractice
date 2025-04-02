b2bd7884464a4c39eba48a1e113eab675ff1c2bb
Anjanee Wijewardana <anjaneew@gmail.com>
2025-04-02 12:21:52 +0300

    Lesson 5 Type Conversion

---
+++ 5/index.js
@@ -0,0 +1,17 @@
+// let age = window.prompt("How old are you? ");
+// age = Number(age);
+// age += 1;
+// console.log(age, typeof age);
+
+let x = "Pizza";
+let y = "Pizza";
+let z = "Pizza";
+
+x = Number(x);
+console.log(x, typeof x);
+
+y = String(y);
+console.log(y, typeof y);
+
+z = Boolean(z);
+console.log(z, typeof z);
