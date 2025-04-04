8c1f564b6eb0f9c608ef0797dd5fe5583517009d
Anjanee Wijewardana <anjaneew@gmail.com>
2025-04-04 19:44:01 +0300

    Lesson 12 Ternary Operators

---
+++ 12/index.js
@@ -0,0 +1,16 @@
+//TERNARY Operators
+
+// SET 1
+let age = 19;
+let message = age >= 18 ? "You are an adult." : "You are a minor.";
+console.log(message);
+
+// SET 2
+let time = 15;
+let greeting = time >= 12 ? "Good afternoon!" : "Good morning!" ;
+console.log(greeting);
+
+// SET 3
+let isStudent = true ;
+let enrolmentStatus = isStudent ? `You are a student` : `You are not enrolled yet` ;
+console.log(enrolmentStatus);
