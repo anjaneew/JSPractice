b9dede80fe26782f6e4669753ee1d5502d0d48c1
Anjanee Wijewardana <anjaneew@gmail.com>
2025-04-07 15:43:21 +0300

    Lesson 17 Logical Operators

---
+++ 17/index.js
@@ -0,0 +1,32 @@
+//AND
+
+const tem = 30;
+
+if (tem > 20 && tem < 32){
+    console.log("The weather is good.");
+}
+else {
+    console.log("The weather is NOT good.");
+}
+
+
+// OR
+const age = 20;
+
+if (age < 16 || age > 80) {
+    console.log("Please contact your teacher.");
+}
+else{
+    console.log("You can enter this site" );
+}
+
+
+//NOT
+const isSunny = true;
+
+if (!isSunny){
+    console.log("it is bad weather here");
+}
+else {
+    console.log("take your umbrella");
+}
