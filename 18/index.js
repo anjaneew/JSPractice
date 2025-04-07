dc6f4164e877a5c9e75d45a5356707bc88e1a2e2
Anjanee Wijewardana <anjaneew@gmail.com>
2025-04-07 15:52:33 +0300

    Lesson 18 Strict Equality Operator

---
+++ 18/index.js
@@ -0,0 +1,34 @@
+const PI = 3.14;
+
+//== Equality Operator
+if (PI == "3.14"){
+    console.log("This is PI.");
+}
+else {
+    console.log("This is NOT PI.");
+}
+
+//=== Strict Equality Operator
+
+if (PI === "3.14"){
+    console.log("This is PI.");
+}
+else {
+    console.log("This is NOT PI.");
+}
+
+//!= Equality Operator
+if (PI != "3.14"){
+    console.log("This is NOT PI.");
+}
+else {
+    console.log("This is PI.");
+}
+
+//!== Equality Operator
+if (PI !== "3.14"){
+    console.log("This is NOT PI.");
+}
+else {
+    console.log("This is PI.");
+}
