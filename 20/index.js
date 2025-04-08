e30fcb3b3b8a4b021263ab7019f41f32e580f811
Anjanee Wijewardana <anjaneew@gmail.com>
2025-04-08 07:09:59 +0300

    Lesson 20 For Loops

---
+++ 20/index.js
@@ -0,0 +1,32 @@
+//For Loops
+
+// for (let i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+// for (let i = 0; i <= 10; i+= 2 ){
+//     console.log(i);
+//  }
+//
+// for(let i = 10; i >= 0; i--){
+//     console.log(i);
+// }
+// console.log("Happy New Year!");
+
+// for (let i = 1; i <= 20; i++){
+//     if(i == 9 || i == 18){
+//         continue; //skip the iteration
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+for(let i = 1; i <= 10; i++){
+    if(i == 6){
+        break; //breaks the loop
+    }
+    else{
+        console.log(i)
+    }
+}
