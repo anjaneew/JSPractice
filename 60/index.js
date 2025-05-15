f42ade3bca307e64d6382d9e865eb80556a38f38
Anjanee Wijewardana <anjaneew@gmail.com>
2025-05-15 11:52:14 +0300

    Lesson 80 Weather App

--- 60/index.js
+++ 60/index.js
@@ -3,15 +3,15 @@
 //code performed concurrently without blocking the execution flow
 //I/O operations, network requests, fetching data,
 //handled with : callbacks, Promises, Async/Await
-
-//Example1
-console.log("Example 1");
-setTimeout(() => console.log("Task 1 delayed "), 1000);
-console.log("Task 2");
-console.log("Task 3");
-console.log("Task 4");
-console.log("Task 5");
-//2 , 3, 4, 5, 1
+//
+// //Example1
+// console.log("Example 1");
+// setTimeout(() => console.log("Task 1 delayed "), 1000);
+// console.log("Task 2");
+// console.log("Task 3");
+// console.log("Task 4");
+// console.log("Task 5");
+// //2 , 3, 4, 5, 1
 
 //Example1
 
@@ -23,10 +23,12 @@ function func1(callback){
 }
 
 function func2(){
-    console.log("Task 7");
-    console.log("Task 8");
-    console.log("Task 9");
-    console.log("Task 10");
+   setTimeout(()=>{
+       console.log("Task 7");
+       console.log("Task 8");
+       console.log("Task 9");
+       console.log("Task 10");
+   }, 3000)
 }
 
 func1(func2);
