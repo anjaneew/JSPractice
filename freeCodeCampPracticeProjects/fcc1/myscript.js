da3c0180dc03247eb1ec16dbfe9110055ea65735
Anjanee Wijewardana <anjaneew@gmail.com>
2025-05-16 21:19:08 +0300

    adding fcc

---
+++ freeCodeCampPracticeProjects/fcc1/myscript.js
@@ -0,0 +1,11 @@
+// Diamond Generator
+//arrays, strings, functions, loops, if/else statements
+
+const character = "!";
+const count = 10;
+const rows = [];
+let inverted = false;
+
+function padRow(rowNumber, rowCount){
+    rowCount - rowNumber
+}
