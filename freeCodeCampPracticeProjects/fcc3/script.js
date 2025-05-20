abc2744b168f69aa7ce3a747d94305a49ce3fb5f
Anjanee Wijewardana <anjaneew@gmail.com>
2025-05-20 17:05:24 +0300

    Project 3 Role Playing game

--- freeCodeCampPracticeProjects/fcc3/script.js
+++ freeCodeCampPracticeProjects/fcc3/script.js
@@ -17,6 +17,7 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons =[{name: "stick", power: 5},{name: "dagger", power: 30},{name: "claw hammer", power: 50}, {name: "sword", power: 100}];
 const locations = [
         {name:"town square",
         "button text" : ["Go to store", "Go to cave", "Fight dragon"], "button functions": [ goStore, goCave, fightDragon] , 
@@ -62,9 +63,33 @@ function fightDragon(){
 
 function buyHealth(){
 
+    if(gold>=10){
+        gold -= 10;
+        health += 10;
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    }
+    else{
+        text.innerText = "You do not have enough gold to buy health.";
+    }
+    
 }
 
 function buyWeapon(){
+    if(currentWeaponIndex<3){
+        if(gold >= 30){
+        gold -= 30;
+        currentWeaponIndex++;
+        goldText.innerText = gold;
+        let newWeapon = weapons[currentWeaponIndex].name;
+        text.innerText = "You now have a " + newWeapon + ".";
+        inventory.push(newWeapon);
+        text.innerText += " In your inventory you have: " + inventory;
+    }
+    else{
+         text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+    }
     
 }
 
