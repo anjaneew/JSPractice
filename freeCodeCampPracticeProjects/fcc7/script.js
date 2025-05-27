fd24628b6a317d8d099ade80069cafd471c2b565
Anjanee Wijewardana <anjaneew@gmail.com>
2025-05-27 10:15:48 +0300

    Project 7 Music Player App

--- freeCodeCampPracticeProjects/fcc7/script.js
+++ freeCodeCampPracticeProjects/fcc7/script.js
@@ -119,6 +119,9 @@ const playSong = id => {
     }
     userData.currentSong = song;
     playButton.classList.add("playing");
+    highlightCurrentSong();
+    setPlayerDisplay();
+    setPlayButtonAccessibleText();
     audio.play(); 
 };
 
@@ -148,13 +151,65 @@ const playPreviousSong = () => {
   }
 };
 
+/*randomize an array - sort (() => Math.random () - 0.5 );
+  One way to randomize an array of items would be to subtract 0.5 from Math.random() which produces random values that are either positive or negative. This makes the comparison result a mix of positive and negative values, leading to a random ordering of elements.
+
+Example Code
+  const names = ["Tom", "Jessica", "Quincy", "Naomi"];
+  names.sort(() => Math.random() - 0.5);
+*/
+const shuffle = () => {
+  userData?.songs.sort(() => Math.random() - 0.5);
+  userData.currentSong = null;
+  userData.songCurrentTime = 0;
+  renderSongs(userData?.songs);
+  pauseSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
+}
+
+const deleteSong = id => {
+  userData.songs = userData?.songs.filter((song)=> song.id !== id);
+  renderSongs(userData?.songs);
+  highlightCurrentSong();
+  setPlayButtonAccessibleText();
+  if(userData?.currentSong?.id === id){
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+    pauseSong();
+    setPlayerDisplay();
+  }
+};
+
+/*textContent 
+sets the text of a node and allows you to set or retrieve the text content of an HTML element. 
+  Ex: 
+    const element = document.getElementById('example');
+    console.log(element.textContent); 
+    // Output: This is some text content
+*/
+
+const setPlayerDisplay = () => {
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist =  document.getElementById("player-song-artist");
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
+};
+
 const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll(".playlist-song");
   const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
   playlistSongElements.forEach((songEl) => {
     songEl.removeAttribute("aria-current");
   });
+  if(songToHighlight){
+    songToHighlight.setAttribute("aria-current", "true");
+  }
 };
+
+
 
 /* arrow functions () => {} 
 
@@ -234,7 +289,7 @@ const renderSongs = array => {
             <span class="playlist-song-duration">${song.duration}</span>
             </button>
             
-            <button class="playlist-song-delete" aria-label="Delete ${song.title}"> 
+            <button class="playlist-song-delete" aria-label="Delete ${song.title}" onclick="deleteSong(${song.id})"> 
             
             <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
             
@@ -246,6 +301,58 @@ const renderSongs = array => {
 
     playlistSongs.innerHTML = songsHTML;
     //Code after return is inside .map()—return ends each iteration, not the whole renderSongs() function.
+
+    if(userData?.songs.length === 0){
+      const resetButton = document.createElement("button");
+      const resetText = document.createTextNode("Reset Playlist");
+      resetButton.id = "reset";
+      resetButton.ariaLabel = "Reset playlist";
+      //both correct! same as 
+      // resetButton.setAttribute("aria-label","Reset playlist");   
+      
+      resetButton.appendChild(resetText);
+      playlistSongs.appendChild(resetButton);
+      resetButton.addEventListener("click", ()=>{
+        userData.songs = [...allSongs];
+        renderSongs(sortSongs());
+        setPlayButtonAccessibleText();
+        resetButton.remove(); 
+      });
+    }   
+
+
+};
+
+/*createElement() 
+    createElement() is a DOM method you can use to dynamically create an element using JavaScript. To use createElement(), you call it, then pass in the tag name as a string:
+
+        Example Code
+        // syntax
+        document.createElement(tagName)
+
+        // example
+        document.createElement('div')
+        You can also assign it to a variable:
+
+        Example Code
+        const divElement = document.createElement('div')*/
+
+/*createTextNode() method of DOM 
+    The createTextNode() method is used to create a text node. To use it, you call it and pass in the text as a string:
+
+    Example Code
+    document.createTextNode("your text")
+    You can also assign it to a variable:
+
+    Example Code
+    const myText = document.createTextNode("your text")
+*/        
+
+//make application more accessible
+const setPlayButtonAccessibleText = () =>{
+  const song = userData?.currentSong || userData?.songs[0];
+  playButton.setAttribute("aria-label", song?.title ? `Play ${song.title}` : "Play"  );
+
 };
 
 const getCurrentSongIndex = () => {
@@ -263,6 +370,24 @@ playButton.addEventListener("click", () => {
 pauseButton.addEventListener("click", pauseSong);
 nextButton.addEventListener("click", playNextSong);
 previousButton.addEventListener("click", playPreviousSong);
+shuffleButton.addEventListener("click", shuffle);
+audio.addEventListener("ended", () => {
+  const currentSongIndex = getCurrentSongIndex();
+  const nextSongExists = userData?.songs[currentSongIndex + 1] !== undefined;
+  //const nextSongExists = userData.songs.length - 1 > currentSongIndex;
+  if(nextSongExists){
+    playNextSong();
+  } else{
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+    pauseSong();
+setPlayerDisplay();
+highlightCurrentSong();
+setPlayButtonAccessibleText();
+  }
+});
+
+/*"ended" event listener */
 
 /**(?.) Optional chaining - check if it is really there without throwing an error
  * 
