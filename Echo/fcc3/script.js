746233beac499331395d5288045cd75f7b853225
Anjanee Wijewardana <anjaneew@gmail.com>
2025-06-07 20:18:11 +0300

    fcc3 Role Play game

---
+++ Echo/fcc3/script.js
@@ -0,0 +1,23 @@
+//Project 3 FCC Role Playing game
+
+let xp = 0;
+let health = 200;
+let gold = 50;
+let currentWeaponIndex = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monstherName");
+const monshterHealthText = document.querySelector("#monsterHealth");
+const weapons = [{}]
+
+const  = document.querySelector("");
