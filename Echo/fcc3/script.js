8b0f4330e1a79c6f168641ccb0b98b733ec1c959
Anjanee Wijewardana <anjaneew@gmail.com>
2025-06-08 17:20:55 +0300

    Project 3 FCC Role Playing game

--- Echo/fcc3/script.js
+++ Echo/fcc3/script.js
@@ -18,6 +18,8 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monstherName");
 const monshterHealthText = document.querySelector("#monsterHealth");
-const weapons = [{}]
-
-const  = document.querySelector("");
+const weapons = [{name: "stick", power: 5},{name: "dagger", power: 30},{name: "claw hammer", power: 50},{name: "sword", power: 100}];
+const monsthers = [{name: "slime", level: 1, health: 10},{name: "fanged beast", level: 2, health: 20},{name: "dragon", level: 3, health: 30}];
+const locations = [
+                    {name: "", 
+                    "button text": ["",]},];
