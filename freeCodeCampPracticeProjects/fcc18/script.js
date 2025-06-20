9d8565877abff0fb3d710aece625b7ac2db8e949
Anjanee Wijewardana <anjaneew@gmail.com>
2025-06-20 11:35:55 +0300

    Project 18 Shopping Cart

---
+++ freeCodeCampPracticeProjects/fcc18/script.js
@@ -0,0 +1,20 @@
+// Project 18 Shopping Cart
+
+
+// const = document.getElementById("");
+const cartContainer = document.getElementById("cart-container");
+const productsContainer = document.getElementById("products-container");
+const dessertCards = document.getElementById("dessert-card-container");
+
+const cartBtn = document.getElementById("cart-btn");
+const clearCartBtn = document.getElementById("clear-cart-btn");
+
+const totalNumberOfItems = document.getElementById("total-items");
+const cartSubTotal = document.getElementById("subtotal");
+const cartTaxes = document.getElementById("taxes");
+const cartTotal = document.getElementById("total");
+
+const showHideCartSpan = document.getElementById("show-hide-cart");
+let isCartShowing = false;
+
+
