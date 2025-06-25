1201e5f9e10f8b7d4a327d9020e3bd982d5e8d18
Anjanee Wijewardana <anjaneew@gmail.com>
2025-06-25 21:06:11 +0300

    Project 21 Dice game

---
+++ freeCodeCampPracticeProjects/fcc21/script.js
@@ -0,0 +1,14 @@
+//Project 21 Dice game
+
+const listOfAllDice = document.querySelectorAll(".die");
+const scoreInputs = document.getElementById("");
+const scoreSpans = document.getElementById("");
+const roundElement = document.getElementById("current-round");
+const rollsElement = document.getElementById("current-round-rolls");
+const totalScore = document.getElementById("total-score");
+const scoreHistory = document.getElementById("score-history");
+const rollDiceBtn = document.getElementById("roll-dice-btn");
+const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesBtn = document.getElementById("rules-btn");
+const rulesContainer = document.querySelector(".rules-container");
+// const  = document.getElementById("");
