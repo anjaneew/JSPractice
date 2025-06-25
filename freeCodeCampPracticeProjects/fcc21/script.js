e9eb559a4d0ee7bb54e8c69b556f7168adf67b67
Anjanee Wijewardana <anjaneew@gmail.com>
2025-06-25 21:06:56 +0300

    Project 21 Dice game

--- freeCodeCampPracticeProjects/fcc21/script.js
+++ freeCodeCampPracticeProjects/fcc21/script.js
@@ -1,8 +1,8 @@
 //Project 21 Dice game
 
 const listOfAllDice = document.querySelectorAll(".die");
-const scoreInputs = document.getElementById("");
-const scoreSpans = document.getElementById("");
+const scoreInputs = document.getElementById("");//
+const scoreSpans = document.getElementById("");//
 const roundElement = document.getElementById("current-round");
 const rollsElement = document.getElementById("current-round-rolls");
 const totalScore = document.getElementById("total-score");
