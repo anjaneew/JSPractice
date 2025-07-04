a2009f054ab1ca8e1c3ccb035217172b16ee5cf9
Anjanee Wijewardana <anjaneew@gmail.com>
2025-07-04 19:04:13 +0300

    Tea making App

---
+++ Echo/teaApp/script.js
@@ -0,0 +1,20 @@
+//Tea making App
+
+const getGroceries = () => {
+    return new Promise((resolve, reject)=> {
+
+    });
+};
+
+const waterBoil = () => {
+    
+};
+
+const addTeaBag = () => {
+
+};
+
+const pourTea = () => {
+
+};
+
