d30df0da9b21b9ab0a8273470f6294ad86d717ca
Anjanee Wijewardana <anjaneew@gmail.com>
2025-07-11 16:58:45 +0300

    Project 23 fcc Authors page

--- freeCodeCampPracticeProjects/fcc23/script.js
+++ freeCodeCampPracticeProjects/fcc23/script.js
@@ -4,7 +4,7 @@ const authorContainer = document.getElementById("author-container");
 const loadMoreBtn = document.getElementById("load-more-btn");
 
 let startingIndex = 0;
-let endingIndex = 8;
+let endingIndex = 8; 
 let authorDataArr = [];
 
 fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
@@ -20,13 +20,15 @@ const displayAuthors = (authors) => {
         authorContainer.innerHTML += `
         <div id="${index}" class="user-card">
         <h2 class="author-name">${author}</h2>
-        <img class="user-img" src="${image}" alt="${author} avatar" referrerpolicy="no-referrer">
+        <img class="user-img" src="${image}" alt="${author} avatar" referrerpolicy="no-referrer"> 
         <div class="purple-divider"></div>
         <p class="bio">${bio.length > 50 ? bio.slice(0, 50) + '...' : bio} </p>
         <a class="author-link" href=${url} target="_blank">${author}'s author page</a>
         </div>
         
         `;
+
+        //referrerpolicy="no-referrer" trying to bug fix
     });
 };
 
